05d0ee698dbd7143969828f7beb7a466b679f487
Lucas de Souza santos <lucass.santoss210@gmail.com>
2022-04-20 11:38:59 -0300

    subindo aula de react

---
+++ cfb-cursos/my-test/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import logo from '../img/logo-login.png'
+
+function Header()
+{
+    return(
+        <header>
+            <img src={logo} alt='logo login' />
+        </header>
+    )
+}
+
+export default Header
